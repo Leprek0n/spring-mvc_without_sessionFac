bf5989eb3555538fb58ef82929596ff95b46eb9e
Leprek0n <fhrhrhhrueuu@gmail.com>
2021-07-25 11:36:14 +0600

    init

---
+++ 2.2.2. spring_mvc/src/main/java/web/controller/Car.java
@@ -0,0 +1,55 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Car {
+    private String color;
+    private String model;
+    private int series;
+
+    public Car(String color, String model, int series) {
+        this.color = color;
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
---
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -0,0 +1,53 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+@Controller
+public class CarController {
+    public List<Car> makeFiveCar() {
+        Car car1 = new Car("Blue", "Mercedes", 1);
+        Car car2 = new Car("Red", "BMW", 2);
+        Car car3 = new Car("Green", "Audi", 3);
+        Car car4 = new Car("White", "Ferrari", 4);
+        Car car5 = new Car("Black", "Mazda", 5);
+        List<Car> carList = new ArrayList<>();
+        carList.add(car1);
+        carList.add(car2);
+        carList.add(car3);
+        carList.add(car4);
+        carList.add(car5);
+
+        return carList;
+    }
+    @GetMapping("/cars")
+    public String showCarList (@RequestParam(value = "count", required = false) Integer count, Model model) {
+        if (count == null || count >= 5) {
+            model.addAttribute("message",makeFiveCar());
+        } else if (count == 2) {
+            model.addAttribute("message", makeFiveCar().get(0) + "\n" + makeFiveCar().get(1));
+        } else if (count == 3) {
+            model.addAttribute("message",
+                    makeFiveCar().get(0) +
+                            "\n" +
+                            makeFiveCar().get(1) +
+                            "\n" +
+                            makeFiveCar().get(2) );
+
+        } else if (count == 4) {
+            model.addAttribute("message",
+                    makeFiveCar().get(0) +
+                            "\n" +
+                            makeFiveCar().get(1) +
+                            "\n" +
+                            makeFiveCar().get(2) +
+                    "\n" +
+                            makeFiveCar().get(3) );
+        }
+        return "car/cars";
+    }
+}
